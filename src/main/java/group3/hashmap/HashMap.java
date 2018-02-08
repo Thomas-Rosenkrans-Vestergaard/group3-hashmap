b94d8cb0fa5c0111f9b2c75fd91494e312346afe
Thomas <Thomas-Rosenkrans-Vestergaard@users.noreply.github.com>
2018-02-08 15:26:06 +0100

    entryset implementation

--- src/main/java/group3/hashmap/HashMap.java
+++ src/main/java/group3/hashmap/HashMap.java
@@ -36,7 +36,13 @@ public class HashMap<K, V> implements Map<K, V>
 	 * Cached {@link PairSet} that can be returned when the
 	 * {@link HashMap#entrySet()} method is called.
 	 */
-	private PairSet cachePairSet = null;
+	private PairSet cachedPairSet = null;
+
+	/**
+	 * Cached {@link KeySet} that can be returned when the
+	 * {@link HashMap#keySet()} method is called.
+	 */
+	private KeySet cachedKeySet = null;
 
 	/**
 	 * Creates a new empty HashMap.
@@ -571,6 +577,52 @@ public class HashMap<K, V> implements Map<K, V>
 	}
 
 	/**
+	 * Finds the pair matching the key from the provided pair.
+	 *
+	 * @param pair The pair providing the key to match.
+	 * @return The matching pair. Returns null if no pair could be found.
+	 */
+	private Pair<K, V> findFromKey(Pair<K, V> pair)
+	{
+		return findFromKey(pair.hash, pair.key);
+	}
+
+	/**
+	 * Finds the pair matching the provided key.
+	 *
+	 * @param key The key of the pair to find.
+	 * @return The matching pair. Returns null if no pair could be found.
+	 */
+	private Pair<K, V> findFromKey(K key)
+	{
+		return findFromKey(key.hashCode(), key);
+	}
+
+	/**
+	 * Finds the pair matching the provided hash and key.
+	 *
+	 * @param hashCode The hash to find the matching pair from.
+	 * @param key      The key to find the matching pair from.
+	 * @return The matching pair. Returns null if no pair could be found.
+	 */
+	private Pair<K, V> findFromKey(int hashCode, K key)
+	{
+
+		int        bucketIndex = hashCode % buckets.length;
+		Pair<K, V> current     = buckets[bucketIndex];
+
+		while (current != null) {
+			if (current.hash == hashCode && current.key.equals(key)) {
+				return current;
+			}
+
+			current = current.next;
+		}
+
+		return null;
+	}
+
+	/**
 	 * Expands the {@link HashMap} by doubling the <code>capacity</code> and expanding the entries.
 	 */
 	private void expand()
@@ -622,10 +674,10 @@ public class HashMap<K, V> implements Map<K, V>
 	 */
 	@Override public PairSet entrySet()
 	{
-		if (cachePairSet == null)
-			cachePairSet = new PairSet();
+		if (cachedPairSet == null)
+			cachedPairSet = new PairSet();
 
-		return cachePairSet;
+		return cachedPairSet;
 	}
 
 	public class PairSet implements Set<Map.Entry<K, V>>
@@ -650,7 +702,7 @@ public class HashMap<K, V> implements Map<K, V>
 		 */
 		@Override public boolean isEmpty()
 		{
-			return HashMap.this.isEmpty();
+			return HashMap.this.size == 0;
 		}
 
 		/**
@@ -915,7 +967,8 @@ public class HashMap<K, V> implements Map<K, V>
 				}
 			}
 
-			placeInStorage(entry.getKey().hashCode(), entry.getKey(), entry.getValue(), null);
+			K key = entry.getKey();
+			placeInStorage(key.hashCode(), key, entry.getValue(), null);
 			return true;
 		}
 
@@ -1112,6 +1165,385 @@ public class HashMap<K, V> implements Map<K, V>
 	 */
 	@Override public Set<K> keySet()
 	{
-		throw new UnsupportedOperationException();
+		if (cachedKeySet == null)
+			cachedKeySet = new KeySet();
+
+		return cachedKeySet;
+	}
+
+	public class KeySet implements Set<K>
+	{
+		/**
+		 * Returns the number of elements in this set (its cardinality).  If this
+		 * set contains more than <tt>Integer.MAX_VALUE</tt> elements, returns
+		 * <tt>Integer.MAX_VALUE</tt>.
+		 *
+		 * @return the number of elements in this set (its cardinality)
+		 */
+		@Override public int size()
+		{
+			return HashMap.this.size;
+		}
+
+		/**
+		 * Returns <tt>true</tt> if this set contains no elements.
+		 *
+		 * @return <tt>true</tt> if this set contains no elements
+		 */
+		@Override public boolean isEmpty()
+		{
+			return HashMap.this.size == 0;
+		}
+
+		/**
+		 * Returns <tt>true</tt> if this set contains the specified element.
+		 * More formally, returns <tt>true</tt> if and only if this set
+		 * contains an element <tt>e</tt> such that
+		 * <tt>(o==null&nbsp;?&nbsp;e==null&nbsp;:&nbsp;o.equals(e))</tt>.
+		 *
+		 * @param o element whose presence in this set is to be tested
+		 * @return <tt>true</tt> if this set contains the specified element
+		 * @throws NullPointerException When the provided object is null.
+		 */
+		@Override public boolean contains(Object o)
+		{
+			if (o == null)
+				throw new NullPointerException("Null keys not allowed.");
+
+			try {
+				return findFromKey((K) o) == null ? false : true;
+			} catch (ClassCastException e) {
+				return false;
+			}
+		}
+
+		/**
+		 * Returns an iterator over the elements in this set.  The elements are
+		 * returned in no particular order (unless this set is an instance of some
+		 * class that provides a guarantee).
+		 *
+		 * @return an iterator over the elements in this set
+		 */
+		@Override public KeyIterator iterator()
+		{
+			return new KeyIterator();
+		}
+
+		public class KeyIterator implements Iterator<K>
+		{
+
+			/**
+			 * The index of the bucket where the <code>nextEntry</code> is from.
+			 */
+			private int currentBucket;
+
+			/**
+			 * The previously returned entry. The {@link PairSet.PairIterator#hasNext()} method only returns
+			 * <code>true</code> when this field is not null. The next entry is found before returning the current
+			 * entry from the {@link PairSet.PairIterator#next()} method.
+			 */
+			private Pair<K, V> nextEntry;
+
+			/**
+			 * Returns {@code true} if the iteration has more keys.
+			 * (In other words, returns {@code true} if {@link #next} would
+			 * return an key rather than throwing an exception.)
+			 *
+			 * @return {@code true} if the iteration has more keys
+			 */
+			@Override public boolean hasNext()
+			{
+				return nextEntry != null;
+			}
+
+			/**
+			 * Returns the next key in the iteration.
+			 *
+			 * @return the next key in the iteration
+			 * @throws NoSuchElementException if the iteration has no more keys.
+			 */
+			@Override public K next()
+			{
+				if (nextEntry == null)
+					throw new NoSuchElementException();
+
+				Pair<K, V> result = nextEntry;
+				nextEntry = getNextEntry(result);
+				return result.key;
+			}
+
+			/**
+			 * Finds the next available entry from the current element. Returns either <code>current.next</code> or
+			 * the head of the next bucket.
+			 *
+			 * @param current The last returned entry.
+			 * @return Returns the next entry. Returns <code>null</code> if there are no next entries.
+			 */
+			private Pair<K, V> getNextEntry(Pair<K, V> current)
+			{
+				if (current.next == null)
+					return getNextHead();
+
+				return current.next;
+			}
+
+			/**
+			 * Returns the head of the next bucket.
+			 *
+			 * @return The head of the next bucket. Returns <code>null</code> if there are no more heads.
+			 */
+			private Pair<K, V> getNextHead()
+			{
+				for (int x = currentBucket + 1; x < buckets.length; x++) {
+					if (buckets[x] != null) {
+						currentBucket = x;
+						return buckets[x];
+					}
+				}
+
+				return null;
+			}
+		}
+
+		/**
+		 * Returns an array containing all of the elements in this set.
+		 * If this set makes any guarantees as to what order its elements
+		 * are returned by its iterator, this method must return the
+		 * elements in the same order.
+		 * <p>
+		 * <p>The returned array will be "safe" in that no references to it
+		 * are maintained by this set.  (In other words, this method must
+		 * allocate a new array even if this set is backed by an array).
+		 * The caller is thus free to modify the returned array.
+		 * <p>
+		 * <p>This method acts as bridge between array-based and collection-based
+		 * APIs.
+		 *
+		 * @return an array containing all the elements in this set
+		 */
+		@Override public K[] toArray()
+		{
+			int nextIndex = 0;
+			K[] result    = (K[]) new Object[size];
+			for (Pair<K, V> current : buckets) {
+				while (current != null) {
+					result[nextIndex++] = current.key;
+					current = current.next;
+				}
+			}
+
+			return result;
+		}
+
+		/**
+		 * Returns an array containing all of the elements in this set; the
+		 * runtime type of the returned array is that of the specified array.
+		 * If the set fits in the specified array, it is returned therein.
+		 * Otherwise, a new array is allocated with the runtime type of the
+		 * specified array and the size of this set.
+		 * <p>
+		 * <p>If this set fits in the specified array with room to spare
+		 * (i.e., the array has more elements than this set), the element in
+		 * the array immediately following the end of the set is set to
+		 * <tt>null</tt>.  (This is useful in determining the length of this
+		 * set <i>only</i> if the caller knows that this set does not contain
+		 * any null elements.)
+		 * <p>
+		 * <p>If this set makes any guarantees as to what order its elements
+		 * are returned by its iterator, this method must return the elements
+		 * in the same order.
+		 * <p>
+		 * <p>Like the {@link #toArray()} method, this method acts as bridge between
+		 * array-based and collection-based APIs.  Further, this method allows
+		 * precise control over the runtime type of the output array, and may,
+		 * under certain circumstances, be used to save allocation costs.
+		 * <p>
+		 * <p>Suppose <tt>x</tt> is a set known to contain only strings.
+		 * The following code can be used to dump the set into a newly allocated
+		 * array of <tt>String</tt>:
+		 * <p>
+		 * <pre>
+		 *     String[] y = x.toArray(new String[0]);</pre>
+		 * <p>
+		 * Note that <tt>toArray(new Object[0])</tt> is identical in function to
+		 * <tt>toArray()</tt>.
+		 *
+		 * @param a the array into which the elements of this set are to be
+		 *          stored, if it is big enough; otherwise, a new array of the same
+		 *          runtime type is allocated for this purpose.
+		 * @return an array containing all the elements in this set
+		 * @throws ArrayStoreException  if the runtime type of the specified array
+		 *                              is not a supertype of the runtime type of every element in this
+		 *                              set
+		 * @throws NullPointerException if the specified array is null
+		 */
+		@Override public <T> T[] toArray(T[] a)
+		{
+			if (a == null)
+				throw new NullPointerException("Array cannot be null.");
+
+			int size = size();
+			T[] r = a.length >= size ? a :
+					(T[]) java.lang.reflect.Array
+							.newInstance(a.getClass().getComponentType(), size);
+			KeyIterator it = iterator();
+
+			int nextIndex = 0;
+			while (it.hasNext()) {
+				r[nextIndex] = (T) it.next();
+				nextIndex++;
+			}
+
+			return r;
+		}
+
+		/**
+		 * Not supported.
+		 *
+		 * @throws UnsupportedOperationException Since the method us not
+		 *                                       supported.
+		 */
+		@Override public boolean add(K k)
+		{
+			throw new UnsupportedOperationException();
+		}
+
+		/**
+		 * Removes the specified element from this set if it is present
+		 * (optional operation).  More formally, removes an element <tt>e</tt>
+		 * such that
+		 * <tt>(o==null&nbsp;?&nbsp;e==null&nbsp;:&nbsp;o.equals(e))</tt>, if
+		 * this set contains such an element.  Returns <tt>true</tt> if this set
+		 * contained the element (or equivalently, if this set changed as a
+		 * result of the call).  (This set will not contain the element once the
+		 * call returns.)
+		 *
+		 * @param o object to be removed from this set, if present
+		 * @return <tt>true</tt> if this set contained the specified element
+		 * @throws NullPointerException When the specified object is
+		 *                              null.
+		 */
+		@Override public boolean remove(Object o)
+		{
+			return HashMap.this.remove(o) != null ? true : false;
+		}
+
+		/**
+		 * Returns <tt>true</tt> if this set contains all of the elements of the
+		 * specified collection.  If the specified collection is also a set, this
+		 * method returns <tt>true</tt> if it is a <i>subset</i> of this set.
+		 *
+		 * @param c collection to be checked for containment in this set
+		 * @return <tt>true</tt> if this set contains all of the elements of the
+		 * specified collection
+		 * @throws NullPointerException When encountering a null element in the provided collection.
+		 * @see #contains(Object)
+		 */
+		@Override public boolean containsAll(Collection<?> c)
+		{
+			if (c == null)
+				throw new NullPointerException();
+
+			for (int x = 0; x < buckets.length; x++) {
+				Pair<K, V> current = buckets[x];
+				while (current != null) {
+					if (!c.contains(current.key)) {
+						return false;
+					}
+
+					current = current.next;
+				}
+			}
+
+			return true;
+		}
+
+		/**
+		 * Unsupported operation.
+		 *
+		 * @throws UnsupportedOperationException
+		 */
+		@Override public boolean addAll(Collection<? extends K> c)
+		{
+			throw new UnsupportedOperationException();
+		}
+
+
+		/**
+		 * Retains only the elements in this set that are contained in the
+		 * specified collection (optional operation).  In other words, removes
+		 * from this set all of its elements that are not contained in the
+		 * specified collection.  If the specified collection is also a set, this
+		 * operation effectively modifies this set so that its value is the
+		 * <i>intersection</i> of the two sets.
+		 *
+		 * @param c collection containing elements to be retained in this set
+		 * @return <tt>true</tt> if this set changed as a result of the call
+		 * @throws NullPointerException If the collection c is
+		 *                              <code>null</code>.
+		 * @see #remove(Object)
+		 */
+		@Override public boolean retainAll(Collection<?> c)
+		{
+			if (c == null)
+				throw new NullPointerException();
+
+			boolean changed = false;
+
+			for (int x = 0; x < buckets.length; x++) {
+				Pair<K, V> current  = buckets[x];
+				Pair<K, V> previous = null;
+				while (current != null) {
+					if (!c.contains(current.key)) {
+						if (previous != null)
+							previous.setNext(current.next);
+						else
+							buckets[x] = null;
+						size--;
+						changed = true;
+					}
+
+					previous = current;
+					current = current.next;
+				}
+			}
+
+			return changed;
+		}
+
+		/**
+		 * Removes from this set all of its elements that are contained in the
+		 * specified collection (optional operation).  If the specified
+		 * collection is also a set, this operation effectively modifies this
+		 * set so that its value is the <i>asymmetric set difference</i> of
+		 * the two sets.
+		 *
+		 * @param c collection containing elements to be removed from this set
+		 * @return <tt>true</tt> if this set changed as a result of the call
+		 * @throws NullPointerException When the provided collection
+		 *                              c is null.
+		 * @see #remove(Object)
+		 * @see #contains(Object)
+		 */
+		@Override public boolean removeAll(Collection<?> c)
+		{
+			boolean changed = false;
+			for (Object o : c) {
+				if (remove(o)) {
+					changed = true;
+				}
+			}
+
+			return changed;
+		}
+
+		/**
+		 * Removes all of the elements from this set (optional operation).
+		 * The set will be empty after this call returns.
+		 */
+		@Override public void clear()
+		{
+			HashMap.this.clear();
+		}
 	}
 }
